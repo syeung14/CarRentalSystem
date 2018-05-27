188b93ec868c43b4843ea45bcab85a2ca4790dc7
Marco Yeung <yeungsangsin@gmail.com>
2018-05-27 12:30:15 -0400

    kick off design project for CRD interview

--- src/test/java/com/carrental/CarRentalSystemServiceTest.java
+++ src/test/java/com/carrental/CarRentalSystemServiceTest.java
@@ -1,5 +1,18 @@
 package com.carrental;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class CarRentalSystemServiceTest {
 
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testCustomerAccountCreation() {
+		
+		
+	}
 }
